834a0996af4e27e4b43b8e898cb9b57d09f64375
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-15 20:38:23 +0600

    done add card for service

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 
 const Home = () => {
     // get data from mongodb and display to UI
@@ -15,6 +16,14 @@ const Home = () => {
             
             <h2 className='text-3xl' > Home page</h2>
             <h2 className='text-3xl' > Total Services {services.length} </h2>
+            <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3" >
+                {
+                    services.map( service => <Service 
+                    key={service._id}
+                    service = {service}
+                    /> )
+                }
+            </div>
         </div>
     );
 };
