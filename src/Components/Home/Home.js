f921cb547f0629a9328acc768222a5b04230d15b
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-15 14:19:48 +0600

    get data from mongodb and show display UI

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    // get data from mongodb and display to UI
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/service`)
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, []); 
+    console.log(services);
     return (
         <div>
             
             <h2 className='text-3xl' > Home page</h2>
+            <h2 className='text-3xl' > Total Services {services.length} </h2>
         </div>
     );
 };
