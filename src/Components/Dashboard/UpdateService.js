ba3f349d79b42811ad781e6c2409deafff9d7ad7
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-19 22:50:22 +0600

    Update service initialized

---
+++ src/Components/Dashboard/UpdateService.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UpdateService = () => {
+    return (
+        <div>
+            <h2 className='text-3xl' > Update Page</h2>
+            
+        </div>
+    );
+};
+
+export default UpdateService;
